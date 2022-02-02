4e14740df6bdf3573e0214135d0fa73c5f7becef
Ferdinando Letizia <ferdinando.letizia@outlook.it>
2022-02-02 19:41:58 +0100

    jumbotron

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,13 @@
 <footer>
-    
+    <section class="footer-body">
+        <div class="container">
+        <img src="{{asset('..images/dc-logo-bg.png')}}" alt="DC Logo" id="logo-footer">
+            <div class="container-list">
+                
+
+        
+            </div>
+
+        </div>
+    </section>
 </footer>
