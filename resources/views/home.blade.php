1683574548addc773fd9b2e92c2fc2e38a48e4e1
Ferdinando Letizia <ferdinando.letizia@outlook.it>
2022-02-02 17:22:23 +0100

    push header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>DC Comics</title>
-</head>
-<body>
-    @include('partials.header')
-    @include('partials.jumbotron')
-    @include('partials.main')
-    @include('partials.footer')
-</body>
-</html>
+@extends('layouts.base')
+
+@section('pageTitle')
+    Home
+@endsection
+
+@section('pageContent')
+    @include('../partials.jumbotron')
+    @include('../partials.main')
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,22 @@
+<header>
+    <div id="header-topbar">
+        <div class="container">
+            <ul>
+                <li>
+                    <a href="/">DC POWER VISA</a>
+                </li>
+                <li>
+                    <a href="/">ADDITIONAL DC SITES</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div id="header">
+        <div class="container">
+            <a href="/">
+                <img src="{{asset('images/dc-logo.png')}}" alt="DC Comincs Logo">
+            </a>
+        </div>
+
+    </div>
+</header>
